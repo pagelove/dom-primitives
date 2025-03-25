98fff7292ab20c39a4f003917f167de2e98bbe5d
James A. Duncan <james@kinburnstreet.consulting>
2025-03-25 08:06:03 +0000

    Much better handling of DAS awareness.

--- index.mjs
+++ index.mjs
@@ -1,5 +1,7 @@
-// might as well return 'this'
+window.location.server = { DASAware: false }
+
 Object.defineProperty(HTMLElement.prototype, "GET", {
+    configurable: true,
     value: function() {
         return this;
     }
@@ -11,53 +13,48 @@ const notDOMAware = function() {
 }
 
 Object.defineProperty(HTMLElement.prototype, 'HEAD', {
+    configurable: true,
     value: notDOMAware
 });
 
 // POST is the HTTP equivalent of appendChild
 Object.defineProperty(HTMLElement.prototype, "POST", {
+    configurable: true,
     value: notDOMAware
 })
 
 // PUT is the HTTP equivalent of replaceChild
 Object.defineProperty(HTMLElement.prototype, "PUT", {
+    configurable: true,
     value: notDOMAware
 });
 
 Object.defineProperty(HTMLElement.prototype, "PATCH", {
+    configurable: true,
     value: notDOMAware
 });
 
 // 
 Object.defineProperty(HTMLElement.prototype, "DELETE", {
+    configurable: true,
     value: notDOMAware
 });
 
-const optionsRequest = fetch(window.location, {
+const optionsRequest = await fetch(window.location, {
     method: "OPTIONS",    
 });
 
-const serverDetail = {}
-Object.defineProperty(serverDetail, "DOMAware", {
-    get: async function() {
-        let result = await optionsRequest;
-        if ( result.ok ) {
-            const dasAvailable = !!result.headers.get('Accept-Ranges').match(/selector/);
-            const evt = new CustomEvent("DASAvailable", { bubbles: true, detail: { }})
-            document.dispatchEvent(evt);
-            return dasAvailable;
-        } else {
-            const evt = new CustomEvent("DASUnavailable", { bubbles: true, detail: { }});
-            document.dispatchEvent(evt);            
-            return false;
-        }
-    }
-})
-Object.defineProperty( window.location, "server", {
-    value: serverDetail
-});
+if ( optionsRequest.ok ) {
+    window.location.server.DASAware = !!optionsRequest.headers.get('Accept-Ranges').match(/selector/);
+}
 
-const { DOMAware } = await window.location.server;
+if (window.location.server.DASAware) {
+    const evt = new CustomEvent("DASAvailable", { bubbles: true, detail: { }})
+    document.dispatchEvent(evt);
+} else {
+    const evt = new CustomEvent("DASUnavailable", { bubbles: true, detail: { }});
+    document.dispatchEvent(evt);            
+}
 
 Object.defineProperty(HTMLElement.prototype, 'selector', {
     enumerable: false,
@@ -72,102 +69,91 @@ Object.defineProperty(HTMLElement.prototype, 'selector', {
     }
 });
 
-
 /*
     If we are a DOM enabled web server then we can do stuff.
 */
-if (DOMAware) {
-    const processResponse = ( anElement, response ) => {
-        if ( response.ok ) {
-            const evt = new CustomEvent("DASOk", { bubbles: true, detail: { element: anElement, response: response }})
-            anElement.dispatchEvent(evt);
-        } else {
-            const evt = new CustomEvent("DASError", { bubbles: true, detail: { element: anElement, response: response }})
-            anElement.dispatchEvent(evt);
-        }
-        return response;
+
+const processResponse = ( anElement, response ) => {
+    if ( response.ok ) {
+        const evt = new CustomEvent("DASOk", { bubbles: true, detail: { element: anElement, response: response }})
+        anElement.dispatchEvent(evt);
+    } else {
+        const evt = new CustomEvent("DASError", { bubbles: true, detail: { element: anElement, response: response }})
+        anElement.dispatchEvent(evt);
     }
-
-    // might as well return 'this'
-    Object.defineProperty(HTMLElement.prototype, "GET", {
-        value: function() {
-            return this;
-        }
-    });
-
-    Object.defineProperty(HTMLElement.prototype, "HEAD", {
-        value: async function() {
-            const headers = new Headers();
-            headers.set('Range', `selector=${this.selector}`);
-            const response = await fetch(window.location, {
-                headers,
-                method: 'HEAD'
-            });
-            return processResponse( this, response )
-        }
-    });
-
-    // POST is the HTTP equivalent of appendChild
-    Object.defineProperty(HTMLElement.prototype, "POST", {
-        value: async function() {
-            const headers = new Headers();
-            headers.set("Range", `selector=${this.parentNode.selector}`);
-            headers.set("Content-Type", "text/html");
-            const response = await fetch(window.location, {
-                headers,
-                body: this.outerHTML,
-                method: POST
-            });
-            return processResponse( this, response );
-        }
-    })
-
-    // PUT is the HTTP equivalent of replaceChild
-    Object.defineProperty(HTMLElement.prototype, "PUT", {
-        value: async function() {
-            const headers = new Headers();
-            headers.set("Range", `selector=${this.selector}`);
-            headers.set("Content-Type", "text/html");
-            const response = await fetch(window.location, {
-                headers,
-                body  : this.outerHTML,
-                method: 'PUT'
-            })
-            return processResponse( this, response );
-        }
-    });
-
-    Object.defineProperty(HTMLElement.prototype, "PATCH", {
-        value: async function( anEMR ) {
-            const headers = new Headers();
-            headers.set('Content-Type', 'application/json');
-            const response = await fetch(window.location, {
-                method: 'PATCH',
-                headers: headers,
-                body: JSON.stringify( anEMR )
-            });
-            return processResponse( this, response );
-        }
-    });
-
-    // 
-    Object.defineProperty(HTMLElement.prototype, "DELETE", {
-        value: async function() {
-            const headers = new Headers();
-            headers.set("Range", `selector=${this.selector}`);
-            const response = await fetch(window.location, {
-                headers,
-                method: 'DELETE'
-            })
-            return processResponse( this, response );
-        }
-    });
-} else {
-    Object.defineProperty(window.location, 'server', {
-        value: {
-            'DOMAware': false
-        }
-    });
-
+    return response;
 }
 
+// might as well return 'this'
+Object.defineProperty(HTMLElement.prototype, "GET", {
+    value: function() {
+        return this;
+    }
+});
+
+Object.defineProperty(HTMLElement.prototype, "HEAD", {
+    value: async function() {
+        const headers = new Headers();
+        headers.set('Range', `selector=${this.selector}`);
+        const response = await fetch(window.location, {
+            headers,
+            method: 'HEAD'
+        });
+        return processResponse( this, response )
+    }
+});
+
+// POST is the HTTP equivalent of appendChild
+Object.defineProperty(HTMLElement.prototype, "POST", {
+    value: async function() {
+        const headers = new Headers();
+        headers.set("Range", `selector=${this.parentNode.selector}`);
+        headers.set("Content-Type", "text/html");
+        const response = await fetch(window.location, {
+            headers,
+            body: this.outerHTML,
+            method: POST
+        });
+        return processResponse( this, response );
+    }
+})
+
+// PUT is the HTTP equivalent of replaceChild
+Object.defineProperty(HTMLElement.prototype, "PUT", {
+    value: async function() {
+        const headers = new Headers();
+        headers.set("Range", `selector=${this.selector}`);
+        headers.set("Content-Type", "text/html");
+        const response = await fetch(window.location, {
+            headers,
+            body  : this.outerHTML,
+            method: 'PUT'
+        })
+        return processResponse( this, response );
+    }
+});
+
+Object.defineProperty(HTMLElement.prototype, "PATCH", {
+    value: async function( anEMR ) {
+        const headers = new Headers();
+        headers.set('Content-Type', 'application/json');
+        const response = await fetch(window.location, {
+            method: 'PATCH',
+            headers: headers,
+            body: JSON.stringify( anEMR )
+        });
+        return processResponse( this, response );
+    }
+});
+
+Object.defineProperty(HTMLElement.prototype, "DELETE", {
+    value: async function() {
+        const headers = new Headers();
+        headers.set("Range", `selector=${this.selector}`);
+        const response = await fetch(window.location, {
+            headers,
+            method: 'DELETE'
+        });
+        return processResponse( this, response );
+    }
+});
